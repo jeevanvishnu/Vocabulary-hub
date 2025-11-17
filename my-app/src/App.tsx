7fb1805faa08f77703ad9a790bf67764b7b60717
jeevanvishnu <jeevanvishnu18@gmail.com>
2025-11-17 15:39:55 +0530

    add backend

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Button from './components/Button'
 import Tables from './components/Tables'
+import Bytez from "bytez.js"
 
 const App = () => {
+  const check = async () => {
+
+    const key = import.meta.env.VITE_Bytez
+    const sdk = new Bytez(key)
+
+    // choose Text-to-Image
+    const model = sdk.model("ZB-Tech/Text-to-Image")
+
+    // send input to model
+    const { error, output } = await model.run("A cat in a wizard hat")
+
+    console.log(error, "...This is an error ");
+    console.log(output, "This is an ouput");
+
+
+  }
+
+
+  useEffect(() => {
+    check()
+  }, [])
+
   return (
     <div>
       <Button />
